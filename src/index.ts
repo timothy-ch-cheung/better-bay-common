aed9c08ec419ba0f2c2df7e07b85fa4cf846738f
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2022-10-13 00:14:55 +0100

    Use named export

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import EbayAuthToken from "ebay-oauth-nodejs-client"
 
 const EBAY_BASE_URL = 'https://api.ebay.com/buy/browse/v1/item';
 
-export default class BetterBayClient {
+export class BetterBayClient {
 
     _ebayAuthToken;
     _token: string;
